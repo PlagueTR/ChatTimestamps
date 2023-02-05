c828a1a149f5e847740c387361543d07c644d52f
PlagueTR <black.plague@icloud.com>
2023-02-05 03:36:41 +0300

    Support for Minecraft 1.19

--- src/main/java/space/plague/chattimestamps/mixin/MixinChatHud.java
+++ src/main/java/space/plague/chattimestamps/mixin/MixinChatHud.java
@@ -6,7 +6,6 @@ import space.plague.chattimestamps.config.GeneralOptions;
 import net.minecraft.client.gui.hud.ChatHud;
 import net.minecraft.client.util.TextCollector;
 import net.minecraft.text.StringVisitable;
-import net.minecraft.text.Text;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -16,7 +15,7 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class MixinChatHud {
 
     //modify message in addMessage(Text message, MessageSignatureData signature, int ticks, MessageIndicator indicator, boolean refresh) method
-    @ModifyArg(method = "addMessage(Lnet/minecraft/text/Text;Lnet/minecraft/network/message/MessageSignatureData;ILnet/minecraft/client/gui/hud/MessageIndicator;Z)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/client/font/TextRenderer;)Ljava/util/List;"), index = 0)
+    @ModifyArg(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/client/font/TextRenderer;)Ljava/util/List;"), index = 0)
     private StringVisitable modifyMessage(StringVisitable message) {
 
         //if mod is disabled, don't change anything
