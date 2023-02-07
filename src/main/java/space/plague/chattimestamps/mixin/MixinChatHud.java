2522ce3f280f806ba3dfb1a9d0a2af70b2d0440a
PlagueTR <black.plague@icloud.com>
2023-02-07 11:54:01 +0300

    Added support for 1.16.2 to 1.16.5

--- src/main/java/space/plague/chattimestamps/mixin/MixinChatHud.java
+++ src/main/java/space/plague/chattimestamps/mixin/MixinChatHud.java
@@ -1,6 +1,7 @@
 package space.plague.chattimestamps.mixin;
 
-import net.minecraft.text.StringRenderable;
+import net.minecraft.text.StringVisitable;
+import net.minecraft.text.Text;
 import space.plague.chattimestamps.Main;
 import space.plague.chattimestamps.config.GeneralOptions;
 
@@ -15,8 +16,8 @@ import org.spongepowered.asm.mixin.injection.ModifyArg;
 public abstract class MixinChatHud {
 
     //modify message in addMessage(Text message, MessageSignatureData signature, int ticks, MessageIndicator indicator, boolean refresh) method
-    @ModifyArg(method = "addMessage(Lnet/minecraft/text/StringRenderable;IIZ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/StringRenderable;ILnet/minecraft/client/font/TextRenderer;)Ljava/util/List;"), index = 0)
-    private StringRenderable modifyMessage(StringRenderable message) {
+    @ModifyArg(method = "addMessage(Lnet/minecraft/text/Text;IIZ)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/client/font/TextRenderer;)Ljava/util/List;"), index = 0)
+    private StringVisitable modifyMessage(StringVisitable message) {
 
         //if mod is disabled, don't change anything
         if (!GeneralOptions.disableMod) {
@@ -24,7 +25,7 @@ public abstract class MixinChatHud {
             TextCollector tc = new TextCollector();
 
             //get formatted timestamp and add 'reset formatting modifier' to the end of timestamp
-            tc.add(StringRenderable.plain(Main.getFormattedTimestamp() + "§r"));
+            tc.add(Text.of(Main.getFormattedTimestamp() + "§r"));
 
             //add the rest of the message to the timestamp
             tc.add(message);
