962fb46ad34060e1d1e3e814e7596b347fb7f8b2
PlagueTR <116841534+PlagueTR@users.noreply.github.com>
2026-06-07 03:25:45 +0300

    Updated to 1.21.11 - Gradle 9.5.1

--- README.md
+++ README.md
@@ -4,7 +4,7 @@
 [![GitHub license: MIT](https://img.shields.io/badge/license-MIT%20License-green)](https://raw.githubusercontent.com/PlagueTR/ChatTimestamps/main/LICENSE)
 ![Environment: Client](https://img.shields.io/badge/environment-client-blue)
 [![Mod loader: Fabric](https://img.shields.io/badge/modloader-Fabric-blue)][fabric]
-![Version](https://img.shields.io/badge/version-1.0.3%2B1.21.x-blue)
+![Version](https://img.shields.io/badge/version-1.0.3%2B1.21.11-blue)
 
 A Fabric mod that adds customizable timestamps to chat!
 
--- src/main/java/space/plague/chattimestamps/mixin/MixinChatHud.java
+++ src/main/java/space/plague/chattimestamps/mixin/MixinChatHud.java
@@ -3,37 +3,36 @@ package space.plague.chattimestamps.mixin;
 import space.plague.chattimestamps.Main;
 
 import net.minecraft.client.gui.hud.ChatHud;
-import net.minecraft.client.util.TextCollector;
-import net.minecraft.text.StringVisitable;
+import net.minecraft.client.gui.hud.ChatHudLine;
+import net.minecraft.text.Text;
 
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.ModifyArg;
+import org.spongepowered.asm.mixin.injection.ModifyVariable;
+
+import static net.minecraft.text.Text.*;
 
 @Mixin(ChatHud.class)
 public abstract class MixinChatHud {
 
     //modify message in addMessage(Text message, MessageSignatureData signature, int ticks, MessageIndicator indicator, boolean refresh) method
-    @ModifyArg(method = "addVisibleMessage(Lnet/minecraft/client/gui/hud/ChatHudLine;)V", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/util/ChatMessages;breakRenderedChatMessageLines(Lnet/minecraft/text/StringVisitable;ILnet/minecraft/client/font/TextRenderer;)Ljava/util/List;"), index = 0)
-    private StringVisitable modifyMessage(StringVisitable message) {
+    @ModifyVariable(method = "Lnet/minecraft/client/gui/hud/ChatHud;addVisibleMessage(Lnet/minecraft/client/gui/hud/ChatHudLine;)V", at = @At(value = "HEAD"), argsOnly = true)
+    private ChatHudLine modifyMessage(ChatHudLine message) {
 
         //if mod is disabled, don't change anything
         if (Main.getConfig().isEnableMod()) {
 
-            TextCollector tc = new TextCollector();
-
-            //get formatted timestamp and add 'reset formatting modifier' to the end of timestamp
-            tc.add(StringVisitable.plain(Main.getFormattedTimestamp() + "§r"));
-
-            //add the rest of the message to the timestamp
-            tc.add(message);
-
-            //set argument to stamped text
-            message = tc.getCombined();
+            message = new ChatHudLine(message.creationTick(),
+                    Text.empty().append(Text.of(Main.getFormattedTimestamp() + "§r")).append(message.content()),
+                    message.signature(),
+                    message.indicator()
+            );
+            Main.LOGGER.info(Main.getFormattedTimestamp() + "§r" + message.content());
+            Main.LOGGER.info(of(Main.getFormattedTimestamp() + "§r" + message.content()));
 
         }
-        return message;
 
+        return message;
     }
 
 }
