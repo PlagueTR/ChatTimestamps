e9a87de7d59d2435f501b1c56521b39c7839c308
PlagueTR <116841534+PlagueTR@users.noreply.github.com>
2026-06-15 18:22:17 +0300

    Uptade to 1.17.1

--- forge/src/main/java/space/plague/chattimestamps/forge/MainForge.java
+++ forge/src/main/java/space/plague/chattimestamps/forge/MainForge.java
@@ -1,21 +1,32 @@
 package space.plague.chattimestamps.forge;
 
-import net.minecraftforge.fml.ExtensionPoint;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fmlclient.ConfigGuiHandler;
+
+import net.minecraft.client.MinecraftClient;
+import net.minecraft.client.gui.screen.Screen;
 
 import space.plague.chattimestamps.Main;
 import space.plague.chattimestamps.config.gui.GeneralOptionsScreen;
+
+import java.util.function.BiFunction;
 
 @Mod(Main.MOD_ID)
 public final class MainForge {
     public MainForge() {
 
-        if (ModList.get().isLoaded("cloth-config")) {
+        if (ModList.get().isLoaded("cloth_config")) {
             ModLoadingContext.get().registerExtensionPoint(
-                    ExtensionPoint.CONFIGGUIFACTORY,
-                    () -> (minecraft, parent) -> GeneralOptionsScreen.getConfigBuilder().build());
+                    ConfigGuiHandler.ConfigGuiFactory.class,
+                    () -> new ConfigGuiHandler.ConfigGuiFactory(new BiFunction<MinecraftClient, Screen, Screen>() {
+                        @Override
+                        public Screen apply(MinecraftClient minecraft, Screen parent) {
+                            return GeneralOptionsScreen.getConfigBuilder().build();
+                        }
+                    })
+            );
         }
 
         // Run our common setup.
