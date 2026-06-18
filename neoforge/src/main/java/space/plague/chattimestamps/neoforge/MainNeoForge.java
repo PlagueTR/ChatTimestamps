a0870e6a116f4800a8006abeef84e28888ac98cd
PlagueTR <116841534+PlagueTR@users.noreply.github.com>
2026-06-18 03:09:32 +0300

    Updated to 1.20.2 & NeoForge

--- neoforge/src/main/java/space/plague/chattimestamps/neoforge/MainNeoForge.java
+++ neoforge/src/main/java/space/plague/chattimestamps/neoforge/MainNeoForge.java
@@ -1,22 +1,22 @@
-package space.plague.chattimestamps.forge;
+package space.plague.chattimestamps.neoforge;
 
-import net.minecraftforge.client.ConfigScreenHandler;
-import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
-import net.minecraftforge.fml.common.Mod;
+import net.neoforged.fml.ModList;
+import net.neoforged.fml.ModLoadingContext;
+import net.neoforged.fml.common.Mod;
 
+import net.neoforged.neoforge.client.ConfigScreenHandler;
 import space.plague.chattimestamps.Main;
 import space.plague.chattimestamps.config.gui.GeneralOptionsScreen;
 
 @Mod(Main.MOD_ID)
-public final class MainForge {
-    public MainForge() {
+public final class MainNeoForge {
+    public MainNeoForge() {
 
         if (ModList.get().isLoaded("cloth_config")) {
             ModLoadingContext.get().registerExtensionPoint(
                     ConfigScreenHandler.ConfigScreenFactory.class,
                     () -> new ConfigScreenHandler.ConfigScreenFactory(
-                            (mc, parentScreen) -> GeneralOptionsScreen.getConfigBuilder().build()
+                            (minecraftClient, screen) ->  GeneralOptionsScreen.getConfigBuilder().build()
                     )
             );
         }
@@ -24,5 +24,4 @@ public final class MainForge {
         // Run our common setup.
         Main.init();
     }
-
 }
