e053b10d0f1c2bf765c5a647cf6d4348ee4833c8
PlagueTR <116841534+PlagueTR@users.noreply.github.com>
2026-06-19 08:01:25 +0300

    Updated to 1.21.1

--- forge/src/main/java/space/plague/chattimestamps/forge/MainForge.java
+++ forge/src/main/java/space/plague/chattimestamps/forge/MainForge.java
@@ -1,23 +1,21 @@
 package space.plague.chattimestamps.forge;
 
 import net.minecraftforge.client.ConfigScreenHandler;
+import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.ModLoadingContext;
 import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
 
 import space.plague.chattimestamps.Main;
 import space.plague.chattimestamps.config.gui.GeneralOptionsScreen;
 
 @Mod(Main.MOD_ID)
 public final class MainForge {
-    public MainForge() {
+    public MainForge(FMLJavaModLoadingContext context) {
 
         if (ModList.get().isLoaded("cloth_config")) {
-            ModLoadingContext.get().registerExtensionPoint(
-                    ConfigScreenHandler.ConfigScreenFactory.class,
-                    () -> new ConfigScreenHandler.ConfigScreenFactory(
-                            (mc, parentScreen) -> GeneralOptionsScreen.getConfigBuilder().build()
-                    )
+            MinecraftForge.registerConfigScreen(
+                    (mc, parentScreen) -> GeneralOptionsScreen.getConfigBuilder().build()
             );
         }
 
